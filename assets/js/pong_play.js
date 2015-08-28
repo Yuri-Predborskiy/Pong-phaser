8e8229b3d7420ef5b58c1e0660aa5c04e46cad96
Yuri Predborskiy <shini1984@gmail.com>
2015-08-28 09:39:40 +0300

    Refactor code for left paddle movement
    Simplify code for moving the left paddle

--- assets/js/pong_play.js
+++ assets/js/pong_play.js
@@ -43,109 +43,22 @@ var playState = {
         // the win sprite is not)
         game.physics.arcade.overlap(this.playerLeft, this.playerRight, this.gameOver, null, this);
 
-        // finally, we give the human player a means of moving
-        // the "player" sprite (WASD to move)
-        // enabling movement along the X axis
-//        keyUP = this.input.keyboard.addKey(Phaser.Keyboard.W);
-//        keyUP.onDown.add(function() { 
-//            this.movePlayer("left", "up", "pressed") 
-//        }, this);
-//        keyUP.onUp.add(function() { 
-//            this.movePlayer("left", "up", "released") 
-//        }, this);
-//        
-//        keyDN = this.input.keyboard.addKey(Phaser.Keyboard.S);
-//        keyDN.onDown.add(function() { 
-//            this.movePlayer("left", "down", "pressed") 
-//        }, this);
-//        keyDN.onUp.add(function() { 
-//            this.movePlayer("left", "down", "released") 
-//        }, this);
-
-        var up_pressed = false;
-        var up_pressed_last = false;
-        var down_pressed = false;
-        var down_pressed_last = false;
-        var total_speed=0;
-
-        if(this.keyboard.isDown(Phaser.Keyboard.A)) {
-            this.playerLeft.body.velocity.x = -175;
-        } else if(this.keyboard.isDown(Phaser.Keyboard.D)) {
-            this.playerLeft.body.velocity.x = 175;
-        } else {
-            this.playerLeft.body.velocity.x = 0;
-        }
-        // enabling movement along Y axis
+        // enabling moving up and down
+        var upVel = 0;
+        var downVel = 0;
         if(this.keyboard.isDown(Phaser.Keyboard.W)) {
-            //this.playerLeft.body.velocity.y += -175;
-            up_pressed = true;
-        } else {
-            up_pressed = false;
-        } 
+            upVel = -175;
+        }
         if(this.keyboard.isDown(Phaser.Keyboard.S)) {
-            //this.playerLeft.body.velocity.y += 175;
-            down_pressed = true;
-        } else {
-            down_pressed = false;
+            downVel = 175;
         }
-        
-        if(up_pressed && !up_pressed_last) {
-            // is pressed, was released
-            total_speed += -175;
-            up_pressed_last = true;
-        } else if(!up_pressed && up_pressed_last) {
-            // is released, was pressed
-            total_speed -= -175;
-            up_pressed_last = false;
-        }
-        
-        if(down_pressed && !down_pressed_last) {
-            // is pressed, was released
-            total_speed += 175;
-            down_pressed_last = true;
-        } else if(!down_pressed && down_pressed_last) {
-            // is released, was pressed
-            total_speed -= 175;
-            down_pressed_last = false;
-        }
-        
-        this.playerLeft.body.velocity.y = total_speed;
+        this.playerLeft.body.velocity.y = upVel + downVel;
         
         game.physics.arcade.collide(this.ball, this.playerLeft);
         game.physics.arcade.collide(this.ball, this.playerRight);
 
     },
     
-    movePlayer: function(player, direction, state) {
-        var speedX = 0;
-        var speedY = 0;
-        if(player == "left") {
-            if(direction == "up") {
-                if(state == "pressed") {
-                    speedY = -175;
-                } else if(state == "released") {
-                    //speedY -= -175;
-                }
-            } else if(direction == "down") {
-                if(state == "pressed") {
-                    speedY = 175;
-                } else if(state == "released") {
-                    //speedY -= 175;
-                }
-            }
-        } else if(player == "right") {
-            var addLater;
-        }
-        //this.playerLeft.body.velocity.y += -175;
-        this.playerLeft.body.velocity.y = speedY;
-        //console.log("total x:",speedX,"y:",speedY);
-        
-    },
-    
-//    moveDN: function(key) {
-//        this.playerLeft.body.velocity.y += 175;
-//    },
-
     gameOver: function() {
         // we start the win state
         game.state.start('gameOver');
